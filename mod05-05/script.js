7d89d50a7c0a763aca6be4c5583013943faa14be
Jean <jeantee24@yahoo.com>
2018-01-04 21:32:00 +0800

    JS Intro 1/4/2018

---
+++ mod05-05/script.js
@@ -0,0 +1,10 @@
+function getSum() {
+	var num = parseInt(document.getElementById("num").value);
+	var result = 0;
+	var counter = 0;
+	while (counter < num){
+		result += counter;
+		counter++;
+	}
+	document.getElementById("show").innerHTML = value
+}

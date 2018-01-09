b3ed9e53c1f8af48cf1dcd2913321b73c68281e4
Jean <jeantee24@yahoo.com>
2018-01-09 21:11:39 +0800

    JS

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+	echo "welcome to PHP programming";
+
+	phpinfo();
+
+	$name = "Jean Ashley Talal"
+
+	print "Good evening. $name";
+
+	echo "Today is" .date("F j, Y"). ".";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"><title>Welcome to PHP programming!</title>
+</head>
+<body>
+
+	<?php
+	// $msg = "Welcome to PHP programming!";
+	// echo "$msg";
+
+	// $username = 'Jane Smith';
+	// echo $username;
+	// echo '<br>';
+	// $username = 'John Doe';
+	// $current_user = $username;
+	// echo $current_user;
+
+	echo 'Buzz Lightyear once said: You\'re a toy!" <br>';
+
+	echo 'You deleted C://*.*<br>';
+	echo "You deleted C://*.*<br>";
+
+	echo "This will not expand: \n a newline";
+
+	?>
+
+
+</body>
+</html>
